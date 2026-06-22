3bef31971355188faccead6db96a395f63e49343
Ratfucker <131674457+Ratfker@users.noreply.github.com>
2026-06-22 16:33:15 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,7 +3,7 @@ public:
     
     vector<int> twoSum(vector<int>& nums, int target) 
     {
-        unordered_map<int, int> seen; // value -> index
+        unordered_map<int, int> seen; 
 
         for (int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
