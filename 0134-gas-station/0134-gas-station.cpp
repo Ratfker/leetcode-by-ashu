ff8e869ad37e61b9a3252604dc937446d4515c24
Ratfucker <131674457+Ratfker@users.noreply.github.com>
2026-06-28 20:37:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    unordered_map<Node*, Node*> visited;
+
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        if (visited.count(node)) return visited[node];
+        Node* clone = new Node(node->val);
+        visited[node] = clone;
+        for (auto neighbor : node->neighbors)
+            clone->neighbors.push_back(cloneGraph(neighbor));
+        return clone;
+    }
+};
