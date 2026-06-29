4728f1292bf877e2a1d16bce76f15b0a0c10b1bc
Ratfucker <131674457+Ratfker@users.noreply.github.com>
2026-06-29 11:47:18 +0530

    Time: 0 ms (100%), Space: 8.2 MB (62.93%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i = 0, j = 0;
+        int n1 = version1.size(), n2 = version2.size();
+
+        while (i < n1 || j < n2) {
+            int v1 = 0, v2 = 0;
+
+            while (i < n1 && version1[i] != '.')
+                v1 = v1 * 10 + (version1[i++] - '0');
+
+            while (j < n2 && version2[j] != '.')
+                v2 = v2 * 10 + (version2[j++] - '0');
+
+            if (v1 < v2) return -1;
+            if (v1 > v2) return 1;
+
+            i++;
+            j++;
+        }
+
+        return 0;
+    }
+};
