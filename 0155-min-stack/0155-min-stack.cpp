a66a8b760c9b53fd0929abd619a922734bd11de6
Ratfucker <131674457+Ratfker@users.noreply.github.com>
2026-06-29 11:38:53 +0530

    Time: 75 ms (9.21%), Space: 151.2 MB (33.5%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,23 @@
+class MinStack {
+    stack<int> st;
+    stack<int> minSt;
+
+public:
+    void push(int val) {
+        st.push(val);
+        minSt.push(minSt.empty() ? val : min(val, minSt.top()));
+    }
+
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+
+    int top() {
+        return st.top();
+    }
+
+    int getMin() {
+        return minSt.top();
+    }
+};
