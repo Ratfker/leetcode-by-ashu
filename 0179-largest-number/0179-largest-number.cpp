1bd717c5d0b78feb616e4d678c68b7d843669301
Ratfucker <131674457+Ratfker@users.noreply.github.com>
2026-06-30 23:35:54 +0530

    Time: 1 ms (85.3%), Space: 17.1 MB (55.79%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> arr;
+        for (int n : nums) arr.push_back(to_string(n));
+        sort(arr.begin(), arr.end(), [](const string& a, const string& b) {
+            return a + b > b + a;
+        });
+        if (arr[0] == "0") return "0";
+        string result;
+        for (string& s : arr) result += s;
+        return result;
+    }
+};
