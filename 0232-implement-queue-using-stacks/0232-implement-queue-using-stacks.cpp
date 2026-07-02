aafe6a508101a832258d821e68402d443f4efa71
Ratfucker <131674457+Ratfker@users.noreply.github.com>
2026-07-02 23:41:24 +0530

    Time: 0 ms (100%), Space: 10.3 MB (5.08%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,37 @@
+class MyQueue {
+public:
+    stack<int> in, out;
+
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        in.push(x);
+    }
+    
+    int pop() {
+        transfer();
+        int val = out.top();
+        out.pop();
+        return val;
+    }
+    
+    int peek() {
+        transfer();
+        return out.top();
+    }
+    
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+
+    void transfer() {
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+    }
+};
