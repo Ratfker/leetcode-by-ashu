082d53d5f306f22fc25d0442fe4bb5c8be7a9dc0
Ratfucker <131674457+Ratfker@users.noreply.github.com>
2026-07-04 23:15:22 +0530

    Time: 3 ms (14.71%), Space: 23.7 MB (82.62%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pos = 0;
+        for (int n : nums)
+            if (n != 0) nums[pos++] = n;
+        while (pos < nums.size()) nums[pos++] = 0;
+    }
+};
