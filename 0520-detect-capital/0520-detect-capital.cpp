24554296612f2142fea20e3d0b7b894e51ddb764
Ratfucker <131674457+Ratfker@users.noreply.github.com>
2026-07-06 00:31:24 +0530

    Time: 0 ms (100%), Space: 8.1 MB (79.96%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.size();
+        int upperCount = 0;
+        for (char c : word) {
+            if (isupper(c)) upperCount++;
+        }
+        if (upperCount == 0 || upperCount == n) return true;
+        if (upperCount == 1 && isupper(word[0])) return true;
+        return false;
+    }
+};
