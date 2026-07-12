18685a841dc0c98d9c8d311ab2f12cf553bafb09
Ratfucker <131674457+Ratfker@users.noreply.github.com>
2026-07-12 18:54:34 +0530

    Time: 0 ms (100%), Space: 7.8 MB (46.44%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+
+        int ans = 10;
+        int current = 9;
+        int available = 9;
+
+        for (int len = 2; len <= n; len++) {
+            current *= available;
+            ans += current;
+            available--;
+        }
+
+        return ans;
+    }
+};
